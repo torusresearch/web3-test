97e6d11fade6dcc65e26969c6b04e61312360ea5
tetratorus <tetratorus@gmail.com>
2019-04-25 15:13:20 +0800

    Add HTTPS redirect

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const forceSsl = require('force-ssl-heroku')
+var app = express();
+app.use(forceSsl);
 const app = express()
+app.use(forceSsl)
 const port = process.env.PORT || 5000
 
 app.use(express.static('public'))
